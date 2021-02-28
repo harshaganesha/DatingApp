53da4c80b978d6386a8332fad1cbf124bcd981c9
harsha ganesha <harshaganesha5@gmail.com>
2021-02-28 20:14:23 +0530

    End of section 2

---
+++ API/Controllers/-- SQLite.sql
@@ -0,0 +1,11 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Harsha");
+
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Varsha");
+
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Sushi");
